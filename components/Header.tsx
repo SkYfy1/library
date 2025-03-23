76f5d854fbdef2810da2b81f50ca14de65e16a52
SkYfy1 <festik.tawer@gmail.com>
2025-03-23 03:28:35 +0200

    header svg

--- components/Header.tsx
+++ components/Header.tsx
@@ -59,20 +59,12 @@ const Header = ({ session }: { session: Session }) => {
         <li>
           <form action={signOutUser}>
             <Button className="bg-transparent text-red-800 hover:bg-transparent shadow-none">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
-                />
-              </svg>
+              <Image
+                src="/icons/logout.svg"
+                alt="Logout"
+                width={24}
+                height={24}
+              />
             </Button>
           </form>
         </li>
